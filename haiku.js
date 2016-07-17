35d7bc52ed6f9d7339b30bb6b2b93edf5fca2603
Rachel Addleman <addlemanrachel@gmail.com>
2016-07-17 09:39:00 -0400

    Updating my haiku generator. Ready to submit

--- haiku.js
+++ haiku.js
@@ -1,9 +1,53 @@
 var fs = require('fs');
 
-
-function createHaiku(structure){
-	console.log("this should log a haiku with the structure " + structure);
+function readFile(file){
+	return fs.readFileSync(file).toString();
 }
+//format the dictionary into an array of sub-arrays sorted by number of syllables.  For example words[3] is an array of all the 3-syllable words in the data
+function formatData(data){
+	var lines = data.toString().split("\n"), 
+		lineSplit,
+		words =[[]];
+	lines.forEach(function(line){
+		lineSplit = line.split("  ");
+		if(lineSplit[1] == null ||lineSplit[1] == undefined || lineSplit[1].match(/\d+/g) === null ){
+			words[0].push(lineSplit[0])
+		} else {
+			var syllab = lineSplit[1].match(/\d+/g).length;
+			if(words[syllab] === undefined){
+				words[syllab] =[];
+			}
+				words[syllab].push(lineSplit[0]);
+		}
+		
+	});
+	return words;
+}
+
+function createHaiku(structure, textFile){
+	var text = readFile('./' + textFile);
+	var textArr = formatData(text);
+	var haikuArr =[];
+	for (var i = 0; i < structure.length; i ++){ 
+		if(typeof structure[i] != "object"){
+		
+		var index = Math.floor(Math.random() * textArr[structure[i]].length -1);
+		haikuArr.push(textArr[structure[i]][index]);
+		} else {
+			var lineArr = [];
+			for (var j = 0; j < structure[i].length; j ++){
+				index = Math.floor(Math.random() * textArr[structure[i][j]].length -1);
+				lineArr.push(textArr[structure[i][j]][index]);
+			}
+			haikuArr.push(lineArr.join(' '));
+		}
+	}
+	return haikuArr.join(" \n");
+		}
+	
+	
+
+
 
 module.exports = {
 	createHaiku: createHaiku,
--- haiku_generator.js
+++ haiku_generator.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var haiku = require('./haiku');
-
-console.log(createHaiku([5,7,5]));
+//createHaiku will take any file in a similar format to the cmuDict, I just haven't found one yet.  It will also take any array of arrays to create different patterns of poems by syllables
+console.log(haiku.createHaiku([5, 7, 5], 'cmuDict.txt'));
